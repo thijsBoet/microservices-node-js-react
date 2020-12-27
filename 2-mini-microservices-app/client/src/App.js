ad6b29620edd87e76f3179baf23afe20a4d62c1e
Matthijs Boet <m.boet@chello.nl>
2020-12-28 00:27:00 +0100

    starting docker

--- 2-mini-microservices-app/client/src/App.js
+++ 2-mini-microservices-app/client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="container">
-      <PostCreate/>
+      <PostCreate />
+      <hr />
+      <PostList/>
     </div>
   );
 }
